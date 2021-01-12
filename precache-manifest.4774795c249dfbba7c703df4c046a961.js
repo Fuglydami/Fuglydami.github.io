9a2da881fb7394a4d12b10596f3a1340ab29c932
Damilare Oyedeji <fuglydami@gmail.com>
2021-01-12 17:41:54 +0100

    Updates

---
+++ precache-manifest.4774795c249dfbba7c703df4c046a961.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b40cfc029d6fc75fcefbf39b6b667b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "747c6c90fb6b3c79d37a",
+    "url": "/static/css/2.ffdfee16.chunk.css"
+  },
+  {
+    "revision": "1e1f30d04383ed23df77",
+    "url": "/static/css/4.3844bd91.chunk.css"
+  },
+  {
+    "revision": "8fe3a479360bbd8dfe58",
+    "url": "/static/css/main.acc2f9d0.chunk.css"
+  },
+  {
+    "revision": "747c6c90fb6b3c79d37a",
+    "url": "/static/js/2.4defcfc2.chunk.js"
+  },
+  {
+    "revision": "573c03b54bf739ab0b9029188d0efb17",
+    "url": "/static/js/2.4defcfc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36fb687ba68afa2be148",
+    "url": "/static/js/3.f7bccd48.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/3.f7bccd48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1f30d04383ed23df77",
+    "url": "/static/js/4.54613417.chunk.js"
+  },
+  {
+    "revision": "8fe3a479360bbd8dfe58",
+    "url": "/static/js/main.a5520d99.chunk.js"
+  },
+  {
+    "revision": "73e168466d2575bff3cb",
+    "url": "/static/js/runtime-main.e3fae308.js"
+  },
+  {
+    "revision": "7562def174e5db8c1fa9fe6ea20244b7",
+    "url": "/static/media/avatar.jpeg.7562def1.jpg"
+  },
+  {
+    "revision": "aea271b930d57b95ca626bb1a9fb27ee",
+    "url": "/static/media/contact.aea271b9.svg"
+  },
+  {
+    "revision": "075fe2c07825b280a4a6c902e7e06631",
+    "url": "/static/media/defaultAvatar.075fe2c0.svg"
+  },
+  {
+    "revision": "ccf2cd437eeafde6685ee8051a7cabae",
+    "url": "/static/media/dummy-project.ccf2cd43.png"
+  },
+  {
+    "revision": "9aece68254c95eca4a0875e213e1d521",
+    "url": "/static/media/focused_working.9aece682.svg"
+  },
+  {
+    "revision": "a1ffbd1e8a162b6afe8c9d971203e2a6",
+    "url": "/static/media/pic1.a1ffbd1e.jpg"
+  },
+  {
+    "revision": "549aa83c6cdc76e8004cbe5c555b5ace",
+    "url": "/static/media/screencapture-birthdayy-reminder-netlify-app-2020-12-24-15_23_29.549aa83c.png"
+  },
+  {
+    "revision": "2c39d3012dd53fa570dd25bcca73b69d",
+    "url": "/static/media/screencapture-celebrity-name-detection-app-herokuapp-2020-12-24-15_21_03.2c39d301.png"
+  },
+  {
+    "revision": "fd668ea20dc98be4937dcc623a954c8f",
+    "url": "/static/media/screencapture-react-tour-app-vercel-app-2020-12-24-15_22_04.fd668ea2.png"
+  }
+]);
