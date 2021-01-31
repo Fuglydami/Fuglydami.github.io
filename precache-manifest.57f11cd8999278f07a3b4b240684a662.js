5f82c92ccdcd2af4ac1cbe79581b82f106b15139
Damilare Oyedeji <fuglydami@gmail.com>
2021-01-31 16:27:00 +0100

    Updates

--- precache-manifest.57f11cd8999278f07a3b4b240684a662.js
+++ precache-manifest.57f11cd8999278f07a3b4b240684a662.js
@@ -1,47 +1,47 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dfe7d717b5671bd0b1d28ab356f504ca",
+    "revision": "d3a4b0e1f57d0eb01c586cc55eeb090e",
     "url": "/index.html"
   },
   {
-    "revision": "cbf5a1e560ecf348a0a4",
+    "revision": "c8dbc16ec3be5eb6b510",
     "url": "/static/css/2.ffdfee16.chunk.css"
   },
   {
-    "revision": "88e6e5e805db071703ec",
+    "revision": "1fa12d89ea8db84e8b6c",
     "url": "/static/css/4.65b673e8.chunk.css"
   },
   {
-    "revision": "d302a01f6b27d74e02a9",
-    "url": "/static/css/main.acc2f9d0.chunk.css"
+    "revision": "351f569a4405fc7e2186",
+    "url": "/static/css/main.668d0f41.chunk.css"
   },
   {
-    "revision": "cbf5a1e560ecf348a0a4",
-    "url": "/static/js/2.cd25c0ee.chunk.js"
+    "revision": "c8dbc16ec3be5eb6b510",
+    "url": "/static/js/2.f6f8489d.chunk.js"
   },
   {
     "revision": "573c03b54bf739ab0b9029188d0efb17",
-    "url": "/static/js/2.cd25c0ee.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.f6f8489d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "14bf1ab6601f6acfcc51",
-    "url": "/static/js/3.aea118a9.chunk.js"
+    "revision": "faca2572fa55786780bd",
+    "url": "/static/js/3.07a04f5e.chunk.js"
   },
   {
     "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
-    "url": "/static/js/3.aea118a9.chunk.js.LICENSE.txt"
+    "url": "/static/js/3.07a04f5e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "88e6e5e805db071703ec",
-    "url": "/static/js/4.457061e9.chunk.js"
+    "revision": "1fa12d89ea8db84e8b6c",
+    "url": "/static/js/4.f5840bb7.chunk.js"
   },
   {
-    "revision": "d302a01f6b27d74e02a9",
-    "url": "/static/js/main.de9db1b8.chunk.js"
+    "revision": "351f569a4405fc7e2186",
+    "url": "/static/js/main.697d797f.chunk.js"
   },
   {
-    "revision": "1151b63f4b0d5a151601",
-    "url": "/static/js/runtime-main.67632041.js"
+    "revision": "a5a2abb7a623b3e4b62b",
+    "url": "/static/js/runtime-main.62f281f7.js"
   },
   {
     "revision": "7562def174e5db8c1fa9fe6ea20244b7",
